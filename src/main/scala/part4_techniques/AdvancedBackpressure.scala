89ce4be5efc8f3e4cfbb192a8b36e023867408de
DESKTOP-4M9HJ2O\ADMIN <minhhieunn@gmail.com>
2020-08-16 16:13:12 +0700

    akka stream fault tolerance and testing

--- src/main/scala/part4_techniques/AdvancedBackpressure.scala
+++ src/main/scala/part4_techniques/AdvancedBackpressure.scala
@@ -33,5 +33,39 @@ object AdvancedBackpressure extends App {
   val notificationSink = Flow[PagerEvent].map(event => Notification(oncallEngineer, event))
     .to(Sink.foreach[Notification](sendEmail))
   // standard
-    eventSource.to(notificationSink).run()
+//    eventSource.to(notificationSink).run()
+
+
+  /*
+  un-backpressurable source
+ */
+
+  def sendEmailSlow(notification: Notification) = {
+    Thread.sleep(1000)
+    println(s"Dear ${notification.email}, you have an event: ${notification.pagerEvent}") // actually send an email
+  }
+
+  val aggregateNotificationFlow = Flow[PagerEvent]
+    .conflate((event1, event2) =>{
+      val nInstances = event1.nInstances + event2.nInstances
+      PagerEvent(s"You have $nInstances events that require your attention", new Date, nInstances)
+    })
+    .map(resultingEvent => Notification(oncallEngineer, resultingEvent))
+
+    eventSource.via(aggregateNotificationFlow).async.to(Sink.foreach[Notification](sendEmailSlow)).run()
+  // alternative to backpressure
+
+  /*
+    Slow producers: extrapolate/expand
+   */
+  import scala.concurrent.duration._
+  val slowCounter = Source(Stream.from(1)).throttle(1, 1 second)
+  val hungrySink = Sink.foreach[Int](println)
+
+  val extrapolator = Flow[Int].extrapolate(element => Iterator.from(element))
+  val repeater = Flow[Int].extrapolate(element => Iterator.continually(element))
+
+  slowCounter.via(repeater).to(hungrySink).run()
+
+  val expander = Flow[Int].expand(element => Iterator.from(element))
 }
